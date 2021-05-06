13ef3b6febcf88398883ee3dba37247e55412bcf
HudsonJr <76413841+Hudsonjr90@users.noreply.github.com>
2021-05-06 00:56:37 -0300

    Update Slider

--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import ImageOne from '../images/house-1.jpg';
 import ImageTwo from '../images/house-2.jpg';
 import ImageThree from '../images/house-3.jpg';
@@ -8,32 +9,26 @@ import ImageSeven from '../images/slide-3.jpg';
 
 export const SliderData = [
   {
-    title: 'Conforto total',
-    price: 'R$3999,90',
-    label href='//www.dt3office.com.br/loja/scuderia/cadeira-de-escritorio-zodiac/'Saiba Mais',
+    title: '',
+    price: '',
     image: ImageOne,
     alt: 'House'
   },
   {
-    title: 'Durabilidade',
-    price: 'R$2999,90',
-    
-    label href='//www.dt3sports.com.br/loja/elite-series/cadeira-gamer-dt3sports-royce-tecido/'Saiba Mais',
+    title: '',
+    price: '',
     image: ImageTwo,
     alt: 'House'
   },
   {
-    title: 'Para o campeão',
-    price: 'R$2999,90',
-    
-    label href='//www.dt3sports.com.br/loja/elite-series/cadeira-gamer-dt3sports-orion-flamengo/'Saiba Mais',
+    title: '',
+    price: '',
     image: ImageThree,
     alt: 'House'
   },
   {
-    title: 'Feita para todos',
-    price: '$3999,90',
-    label href='//www.dt3office.com.br/loja/scuderia/iryna-se/'Saiba Mais',
+    title: '',
+    price: '',
     image: ImageFour,
     alt: 'House'
   }
@@ -41,23 +36,20 @@ export const SliderData = [
 
 export const SliderDataTwo = [
   {
-    
-    
-    label: 'Feita pra durar',
+    title: '',
+    price: '',
     image: ImageFive,
     alt: 'House'
   },
   {
-    
-    
-    label: 'A Melhor Garantia',
+    title: '',
+    price: '',
     image: ImageSix,
     alt: 'House'
   },
   {
-    
-   
-    label: 'Saiba Mais',
+    title: '',
+    price: '',
     image: ImageSeven,
     alt: 'House'
   }
