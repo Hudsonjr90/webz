2632c0939910f06747bc33e879b83885acbb261a
HudsonJr <76413841+Hudsonjr90@users.noreply.github.com>
2021-05-06 00:55:31 -0300

    Update InfoData

--- src/data/InfoData.js
+++ src/data/InfoData.js
@@ -6,20 +6,19 @@ export const InfoData = {
   paragraphOne:
     'A palavra "Ergonomia" vem de duas palavras Gregas: "ergon" que significa trabalho, e "nomos" que significa leis. Conhecida comumente como estudo científico da relação entre o homem e seus ambientes de trabalho, a ergonomia tem alguns objetivos básicos que são: possibilitar o conforto ao indivíduo e proporcionar a prevenção de acidentes e do aparecimento de patologias específicas para determinado tipo de trabalho.',
   paragraphTwo:
-    'Qual o papel da Ergonomia para a saúde?',
-  buttonLabel: 'Veja abaixo',
+    'O ergonomista tem diante de si as seguintes tarefas. A primeira é estudar a ocupação, a fim de determinar o que o operador ou usuário de um determinado equipamento terá de fazer. Em um segundo momento ele deve considerar, como principal na relação com o homem, o que ele tem que ver e ouvir.',
+  
   image: ImageOne,
   reverse: false,
   delay: 100
 };
 
 export const InfoDataTwo = {
-  heading: 'Design e Ergonomia',
+  heading: 'Porque somos os melhores?',
   paragraphOne:
-    'Ideal para reconfigurar o seu espaço de trabalho, a Cadeira de Escritório Zodiac cria um ambiente de trabalho sofisticado e ao mesmo tempo diferenciado, com cores suaves que agregam personalidade.',
+    'Focada em compor um ambiente de trabalho moderno e sofisticado, o Grupo DT3 criou a DT3office, que oferece cadeiras ergonômicas para escritório. Nossos produtos vão além de um assento confortável, eles oferecem uma nova experiência aliando design, tecnologia e ergonomia, para garantir sua saúde e boa postura.',
   paragraphTwo:
-    ' A  Zodiac possui uma variada quantidade de funções e acionamentos, para se adaptar da melhor forma possível às suas necessidades.',
-  buttonLabel: 'Veja mais',
+    'Veja também nossa linha de acessórios',
   image: ImageTwo,
   reverse: true,
   delay: 300
