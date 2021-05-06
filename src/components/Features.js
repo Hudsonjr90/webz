ae8367bc11fe7574606f7b21b64c9716493556b8
HudsonJr <76413841+Hudsonjr90@users.noreply.github.com>
2021-05-06 03:12:49 -0300

    strong up

--- src/components/Features.js
+++ src/components/Features.js
@@ -96,8 +96,8 @@ A formação da classe operária demandou uma série de necessidades que nem sem
               <p>
               Na imagem, temos os anarquistas de Chicago condenados à execução como responsáveis pela explosão da bomba em 04 de maio de 1886
               </p>
-              <strong><button><a href="https://brasilescola.uol.com.br/datas-comemorativas/dia-do-trabalho.htm" target="_blank">
-                 Saiba Mais</a></button></strong>
+              <strong><strong><a href="https://brasilescola.uol.com.br/datas-comemorativas/dia-do-trabalho.htm" target="_blank">
+                 Saiba Mais</a></strong></strong>
             </Content>
           </ColumnLeft>
           <ColumnRight>
